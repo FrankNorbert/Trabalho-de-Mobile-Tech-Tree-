f149ef1acf0d5d3bd66f2cd7fc2a9200d71632f0
jvpts11 <jvptsyt11@gmail.com>
2023-01-08 22:51:50 +0000

    login implemented
    idk if it's working

--- TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/Core/LoginActivity.kt
+++ TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/Core/LoginActivity.kt
@@ -1,12 +1,46 @@
 package Trabalho.Mobile.Core
 
 import Trabalho.Mobile.R
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var email :EditText
+    private lateinit var password:EditText
+    private lateinit var loginButton :Button
+
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        email = findViewById(R.id.login_email_input)
+        password = findViewById(R.id.login_password_input)
+        loginButton = findViewById(R.id.login_button)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener{
+            var txt_email : String = email.text.toString()
+            var txt_password : String = password.text.toString()
+
+            loginUser(txt_email, txt_password)
+        }
+    }
+
+    private fun loginUser(txtEmail: String, txtPassword: String) {
+
+        auth.signInWithEmailAndPassword(txtEmail,txtPassword).addOnSuccessListener(this) { task->
+            Toast.makeText(this, "Login feito com sucesso!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
--- TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/Core/MainActivity.kt
+++ TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/Core/MainActivity.kt
@@ -1,43 +1,12 @@
 package Trabalho.Mobile.Core
 
-
 import Trabalho.Mobile.R
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var botaoLogin : Button
-    private lateinit var botaoRegister: Button
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        botaoLogin = findViewById<Button>(R.id.button_login)
-        botaoRegister = findViewById<Button>(R.id.button_register)
-
-        botaoRegister.setOnClickListener{
-            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        botaoLogin.setOnClickListener{
-            val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(loginIntent)
-            finish()
-        }
-
-        /*
-        botaoLogin.setOnClickListener{
-            setContentView(R.layout.login_screen)
-        }
-        */
     }
 }
