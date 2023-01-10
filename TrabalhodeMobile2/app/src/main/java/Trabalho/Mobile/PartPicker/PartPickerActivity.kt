0970e6cd46734494b79e67de5cae461fbb1656de
jvpts11 <jvptsyt11@gmail.com>
2023-01-10 19:19:03 +0000

    go touch some grass
    now

---
+++ TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/PartPicker/PartPickerActivity.kt
@@ -0,0 +1,12 @@
+package Trabalho.Mobile.PartPicker
+
+import Trabalho.Mobile.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PartPickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_part_picker)
+    }
+}
