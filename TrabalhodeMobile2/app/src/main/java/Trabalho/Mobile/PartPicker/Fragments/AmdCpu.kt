660db288a83429e04e360c683256cad03730f3a8
jvpts11 <jvptsyt11@gmail.com>
2023-01-12 04:34:59 +0000

    i think it's the last commit
    i'm so tired by this point that i can't do anything anymore

--- TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/PartPicker/Fragments/AmdCpu.kt
+++ TrabalhodeMobile2/app/src/main/java/Trabalho/Mobile/PartPicker/Fragments/AmdCpu.kt
@@ -6,55 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import Trabalho.Mobile.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.BaseAdapter
+import android.widget.ListView
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AmdCpu.newInstance] factory method to
- * create an instance of this fragment.
  */
 class AmdCpu : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var cpuList : ListView
+
+    private val cpuAdapter = CpuAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_amd_cpu, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AmdCpu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AmdCpu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cpuList = view.findViewById(R.id.amd_cpu_list)
+        //cpuList.adapter = cpuAdapter
+    }
+
+    internal class CpuAdapter : BaseAdapter(){
+        override fun getCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItem(p0: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemId(p0: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            TODO("Not yet implemented")
+        }
+
     }
 }
